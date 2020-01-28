1d5acf30789a23797ba7e6673fbbf363084f3842
Federico <federicoruhl@gmail.com>
2020-01-27 21:08:59 -0300

    fotos agregadas

---
+++ Gimnasio/ConsultasPerfiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gimnasio
+{
+    public partial class ConsultasPerfiles : FormularioBase
+    {
+        public ConsultasPerfiles()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultasPerfiles_Load(object sender, EventArgs e)
+        {
+            this.tablaPersonaTableAdapter.Fill(this.personaDataSet.tablaPersona);
+
+        }
+    }
+}
